61f5b992f4db5209e61e760ea0088dffb32228f7
Ahmy Yulrizka <yulrizka@users.noreply.github.com>
2021-01-07 18:14:03 +0100

    feat: initial work on websocket

--- cmd/grproxy/main.go
+++ cmd/grproxy/main.go
@@ -5,29 +5,63 @@ import (
 	"flag"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/yulrizka/grproxy"
 )
 
 func main() {
-	addr := ":9999"
+	grpcAddr := ":9999"
+	httpAddr := ":9998"
 	target := "127.0.0.1:10000"
 
-	flag.StringVar(&addr, "addr", env("ADDR", addr), "proxy address")
+	flag.StringVar(&grpcAddr, "grpc-addr", env("GRPC_ADDR", grpcAddr), "proxy address")
+	flag.StringVar(&httpAddr, "http-addr", env("GRPC_ADDR", httpAddr), "proxy address")
 	flag.StringVar(&target, "target", env("TARGET", target), "target grpc server")
 	flag.Parse()
 
-	l, err := net.Listen("tcp", addr)
+	l, err := net.Listen("tcp", grpcAddr)
 	if err != nil {
 		log.Fatalf("listen: %v", err)
 	}
 
-	log.Printf("grproxy %v -> %v", addr, target)
+	log.Printf("grproxy %v -> %v", grpcAddr, target)
+	log.Printf("HTTP UI %v", httpAddr)
 
-	interceptor := grproxy.NewInterceptor(target)
-	if err := interceptor.Serve(context.Background(), l); err != nil {
-		log.Fatal(err)
+	g, ctx := errgroup.WithContext(context.Background())
+	g.Go(func() error {
+		interceptor := grproxy.NewInterceptor(target)
+		return interceptor.Serve(ctx, l)
+	})
+
+	g.Go(func() error {
+		httpHandler := grproxy.NewHTTPHandler()
+		mux := http.NewServeMux()
+		mux.HandleFunc("/ws", httpHandler.HandleWebsocket)
+
+		httpServer := &http.Server{
+			Addr:              httpAddr,
+			ReadHeaderTimeout: 10 * time.Second,
+			WriteTimeout:      10 * time.Second,
+			Handler:           mux,
+		}
+
+		go func() {
+			<-ctx.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			_ = httpServer.Shutdown(ctx)
+		}()
+
+		return httpServer.ListenAndServe()
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatalf(err.Error())
 	}
 }
 
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/gorilla/websocket v1.4.2
 	github.com/jhump/protoreflect v1.7.0
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
 	google.golang.org/grpc v1.32.0
 	google.golang.org/protobuf v1.23.0
 )
